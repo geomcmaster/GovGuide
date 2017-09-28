62e979a7274fe2bbce69795824e88a22a1f87a63
geomcmaster <geomcmaster@gmail.com>
2017-09-27 22:53:09 -0500

    Hamburger icon

--- app/src/main/java/com/example/android/govguide/MainActivity.kt
+++ app/src/main/java/com/example/android/govguide/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.android.govguide
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceManager
@@ -12,8 +15,6 @@ import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
-import com.example.android.govguide.data_objects.Office
-import com.example.android.govguide.data_objects.Official
 import com.example.android.govguide.data_objects.Representatives
 import com.example.android.govguide.utils.Api
 import com.google.gson.Gson
@@ -25,16 +26,24 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    //TODO use lateinit for some of these?
     var reps: Representatives? = null
     val api = Api()
     val sharedPrefChangeListener = { sharedPreferences: SharedPreferences, s: String ->
         getResult()
     }
     var repAdapter = RepAdapter(Representatives(arrayOf(), arrayOf()))
+    lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.drawer_open,
+                R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_drawer)
+        drawerToggle.isDrawerIndicatorEnabled = true
 
         left_drawer.setAdapter(ArrayAdapter<String>(this, R.layout.drawer_list_item, resources.getStringArray(R.array.drawer_options)))
         left_drawer.setOnItemClickListener { adapterView, view, i, l ->
@@ -47,9 +56,7 @@ class MainActivity : AppCompatActivity() {
                 .registerOnSharedPreferenceChangeListener(sharedPrefChangeListener)
 
         drawer_layout
-                .addDrawerListener(
-                        ActionBarDrawerToggle(
-                                this, drawer_layout, R.string.app_name, R.string.app_name))
+                .addDrawerListener(drawerToggle)
 
         rv_reps.layoutManager =
                 LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -60,6 +67,37 @@ class MainActivity : AppCompatActivity() {
             repAdapter = RepAdapter(r)
             rv_reps.adapter = repAdapter
         }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        drawerToggle.syncState()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(sharedPrefChangeListener)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                //TODO get this to work
+//                if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+//                    drawer_layout.openDrawer(GravityCompat.START)
+//                } else {
+//                    drawer_layout.closeDrawer(GravityCompat.START)
+//                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
     }
 
     override fun onContextItemSelected(item: MenuItem?): Boolean {
@@ -112,12 +150,6 @@ class MainActivity : AppCompatActivity() {
         return super.onContextItemSelected(item)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .unregisterOnSharedPreferenceChangeListener(sharedPrefChangeListener)
-    }
-
     /*
     FUNCTIONS FOR MODIFYING VISIBILITY
      */
@@ -126,11 +158,13 @@ class MainActivity : AppCompatActivity() {
         tv_err_msg.visibility = View.VISIBLE
         rv_reps.visibility = View.INVISIBLE
     }
+
     fun showLoading() {
         tv_err_msg.visibility = View.INVISIBLE
         rv_reps.visibility = View.INVISIBLE
         pb_loading_reps.visibility = View.VISIBLE
     }
+
     fun showRecyclerView() {
         pb_loading_reps.visibility = View.INVISIBLE
         tv_err_msg.visibility = View.INVISIBLE
