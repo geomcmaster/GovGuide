8a16e9f048a8fc03859b57cb4623843b7a4d061f
geomcmaster <geomcmaster@gmail.com>
2017-10-01 18:49:39 -0500

    Use location

--- app/src/main/java/com/example/android/govguide/SettingsFragment.kt
+++ app/src/main/java/com/example/android/govguide/SettingsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.android.govguide
 
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
 
 
 /**
@@ -14,19 +22,69 @@ import com.google.android.gms.location.LocationServices
  */
 class SettingsFragment : PreferenceFragmentCompat() {
     var locationClient: FusedLocationProviderClient? = null
+    lateinit var geoCoder: Geocoder
+    val PERMISSIONS_REQUEST_LOCATION = 1
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_LOCATION -> {
+                if (locationClient == null) {
+                    locationClient = LocationServices
+                            .getFusedLocationProviderClient(this.context)
+                }
+                val permission = ContextCompat.checkSelfPermission(this.context,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                if (permission == PackageManager.PERMISSION_GRANTED) {
+                    locationClient?.lastLocation?.addOnSuccessListener { loc ->
+                        doAsync {
+                            setPrefFromLocation(loc)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun setPrefFromLocation(loc: Location) {
+        //TODO get all results and let user select
+        val addressList = geoCoder
+                .getFromLocation(loc.latitude, loc.longitude, 1)
+        if (addressList.size > 0) {
+            val address = "${addressList[0].thoroughfare}, " +
+                    "${addressList[0].locality}, " +
+                    "${addressList[0].adminArea} " +
+                    "${addressList[0].postalCode}"
+            findPreference(getString(R.string.pref_location_key))
+                    .callChangeListener(address)
+        }
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.pref)
+        
+        geoCoder = Geocoder(this.context)
+
         findPreference(getString(R.string.pref_use_curr_loc_key)).onPreferenceClickListener =
                 Preference.OnPreferenceClickListener { _ ->
                     if (locationClient == null) {
-                        locationClient = LocationServices.getFusedLocationProviderClient(this.context)
+                        locationClient = LocationServices
+                                .getFusedLocationProviderClient(this.context)
                     }
-                    //TODO request permission, get location and pass to callChangeListener
-                    //https://developer.android.com/training/location/retrieve-current.html
-                    //https://developer.android.com/training/location/display-address.html
-                    //https://developer.android.com/training/permissions/requesting.html
-                    findPreference(getString(R.string.pref_location_key)).callChangeListener("")
+                    val permission = ContextCompat.checkSelfPermission(this.context,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                    if (permission == PackageManager.PERMISSION_GRANTED) {
+                        locationClient?.lastLocation?.addOnSuccessListener { loc ->
+                            doAsync {
+                                setPrefFromLocation(loc)
+                            }
+                        }
+                    } else {
+                        ActivityCompat.requestPermissions(this.activity,
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                PERMISSIONS_REQUEST_LOCATION)
+                    }
                     true
         }
     }
