79949f8a381806b41a1fa73a9030096764aa107e
geomcmaster <geomcmaster@gmail.com>
2017-10-01 19:31:17 -0500

    Update preference with location

--- app/src/main/java/com/example/android/govguide/SettingsFragment.kt
+++ app/src/main/java/com/example/android/govguide/SettingsFragment.kt
@@ -11,12 +11,12 @@ import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
 
-
+//TODO error message on location retrieval error
 /**
  * A simple [Fragment] subclass.
  */
@@ -48,22 +48,22 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     fun setPrefFromLocation(loc: Location) {
-        //TODO get all results and let user select
+        //TODO get all results and let user select?
         val addressList = geoCoder
                 .getFromLocation(loc.latitude, loc.longitude, 1)
         if (addressList.size > 0) {
-            val address = "${addressList[0].thoroughfare}, " +
+            val address = "${addressList[0].subThoroughfare} ${addressList[0].thoroughfare}, " +
                     "${addressList[0].locality}, " +
                     "${addressList[0].adminArea} " +
                     "${addressList[0].postalCode}"
-            findPreference(getString(R.string.pref_location_key))
-                    .callChangeListener(address)
+            PreferenceManager.getDefaultSharedPreferences(this.context)
+                    .edit().putString(getString(R.string.pref_location_key), address).apply()
         }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.pref)
-        
+
         geoCoder = Geocoder(this.context)
 
         findPreference(getString(R.string.pref_use_curr_loc_key)).onPreferenceClickListener =
